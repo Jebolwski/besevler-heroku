4394ebe84425e4c2d30e8a414b4224217b3ed72c
Mert Gökmen <mertgkmeen@gmail.com>
2022-02-09 20:57:32 +0300

    secrets

--- besevlermuhtarligi/settings.py
+++ besevlermuhtarligi/settings.py
@@ -143,4 +143,4 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = "587"
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "besevler.mah.muh@gmail.com"
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = 'besevlermahmuh16'
