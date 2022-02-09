14c5e41d885fd128068f1d2f71c46f81d175bd64
Mert Gökmen <mertgkmeen@gmail.com>
2022-02-09 17:40:14 +0300

    fixes

--- besevlermuhtarligi/settings.py
+++ besevlermuhtarligi/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l4f2+6+9m1m2qbt9=rz0&ejt9=q*&xhxj9nc7j^qv&!+4qf=8$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = False
 ALLOWED_HOSTS = ['besevlermahallesi.herokuapp.com','127.0.0.1']
 
 
