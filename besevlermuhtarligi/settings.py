7c4af923153f1e678a92a6248e9e3f753b4deb91
Mert Gökmen <mertgkmeen@gmail.com>
2022-02-09 11:35:03 +0300

    third commit

--- besevlermuhtarligi/settings.py
+++ besevlermuhtarligi/settings.py
@@ -120,14 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[BASE_DIR / 'static']
 
-STATIC_ROOT = '/static/'
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images') 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
